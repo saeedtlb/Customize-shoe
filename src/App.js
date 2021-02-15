ee98b280c8b6d571ed0257522b9bf7dbe4d9db2a
saeed-tlb <saeedt7879s@gmail.com>
2021-02-15 13:41:36 +0330

    add Shoe component in the main App file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Canvas } from 'react-three-fiber'
+// shoe
+import Shoe from './Shoe'
+
+const App = () => {
+  return (
+    <Canvas>
+      <Shoe />
+    </Canvas>
+  )
+}
+
+export default App
