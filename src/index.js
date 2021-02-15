3fda4adc51e4258306139a41810e4e02d7ec43a0
saeed-tlb <saeedt7879s@gmail.com>
2021-02-15 18:57:35 +0330

    import css file and render the main App file to the DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './App';
+import App from './App'
+// style
+import './app.css'
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'))
